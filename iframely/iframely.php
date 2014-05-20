fe34b4999727fa7d3ac113db6cb0bb0240e6405d
Ivan Paramonau <i.paramonau@gmail.com>
2014-05-20 13:34:26 -0400

    Bump version

--- iframely/iframely.php
+++ iframely/iframely.php
@@ -4,7 +4,7 @@ Plugin Name: Iframely
 Plugin URI: http://wordpress.org/plugins/iframely/
 Description: Iframely for WordPress. Embed anything, with responsive widgets.
 Author: Itteco Corp.
-Version: 0.2.0
+Version: 0.2.2
 Author URI: http://iframe.ly/?from=wp
 */
 
