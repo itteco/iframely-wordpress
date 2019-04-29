295ee281d42bc1498794c311f48382f61d2f2fd2
garmoncheg <garmon1@gmail.com>
2019-04-29 15:26:49 +0300

    + render options form according to options received + dev process ignores

--- iframely/src/index.js
+++ iframely/src/index.js
@@ -1,5 +1,5 @@
 /**
- * Creates editable block with iframely embed generator
+ * Iramely oembed scripts
  */
 const { __ } = wp.i18n;
 const { createHigherOrderComponent } = wp.compose;
@@ -7,25 +7,66 @@ const { Fragment } = wp.element;
 const { InspectorControls } = wp.editor;
 const { PanelBody } = wp.components;
 
-const withInspectorControls =  createHigherOrderComponent( ( BlockEdit ) => {
+function findIframeByContentWindow(iframes, contentWindow) {
+    let foundIframe;
+    for(let i = 0; i < iframes.length && !foundIframe; i++) {
+        let iframe = iframes[i];
+        if (iframe.contentWindow === contentWindow) {
+            foundIframe = iframe;
+        }
+    }
+    return foundIframe;
+}
 
+window.addEventListener("message",function(e){
+    let frames = document.getElementsByTagName("iframe");
+    if(new RegExp("setIframelyEmbedOptions").test(e.data)) {
+        let iframe = findIframeByContentWindow(frames, e.source);
+        $(iframe).data(JSON.parse(e.data));
+    }
+},false);
+
+class IframelyOptions extends React.Component {
+
+    renderForm(clientId) {
+        // Rendering form for options in the Inspector
+        let selector = 'div#block-' + clientId;
+        let options = $(selector).find('iframe').data();
+        if (options) {
+            iframely.buildOptionsForm(selector, $('div#ifopts').get(0), options.data)
+        }
+    }
+
+    componentDidMount() {
+        this.renderForm(this.props.data);
+    }
+
+    render() {
+        return <div id="ifopts" data-id={ this.props.data }></div>;
+    }
+
+}
+IframelyOptions.defaultProps = {
+    data: '',
+};
+
+const withInspectorControls =  createHigherOrderComponent( ( BlockEdit ) => {
     return ( props ) => {
-        if (props.name === "core/embed" || props.name.startsWith("core-embed")) {
+        if (props.isSelected===true && (props.name === "core/embed" || props.name.startsWith("core-embed"))) {
             return (
                 <Fragment>
-                        <BlockEdit { ...props } />
-                        <InspectorControls>
-                                <PanelBody >
-                                        <div id="ifopts">My custom control</div>
-                                </PanelBody>
-                        </InspectorControls>
+                    <BlockEdit { ...props } />
+                    <InspectorControls>
+                        <PanelBody >
+                            <IframelyOptions data={ props.clientId }/>
+                        </PanelBody>
+                    </InspectorControls>
                 </Fragment>
             );
         } else {
             return (<Fragment><BlockEdit { ...props } /></Fragment>);
         }
     };
-
 }, "withInspectorControl" );
 
 wp.hooks.addFilter( 'editor.BlockEdit', 'iframely/with-inspector-controls', withInspectorControls );
