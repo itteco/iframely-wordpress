4a43c74b93cfde6b7aa99fb3731ca5267745bb03
Alexander Matveyenko <alexander.matveenko@gmail.com>
2022-11-07 18:54:04 +0100

    Fix radio button styles in URL options

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '9ebee8c545b52a878e18');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2b787e75d61c1a84f7e7');
