4076720dd094d1653c7a36ef194516993c472575
Alexander Matveyenko <alexander.matveenko@gmail.com>
2023-12-01 16:49:48 +0100

    Update deps

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2b787e75d61c1a84f7e7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e0ebbb94604e88da9593');
